b2745ed193541b76ba39e606af37c5e4e7cfc640
itsbluejelly <ronniedenzel0@gmail.com>
2024-05-03 18:34:26 +0600

    Finished setup

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-web3-v4"; 
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+};
+
+export default config;
